133f1a8ab9d2ff61de47fa15202d5a430367f844
Ichlas Wardy Gustama <ichlaswardy26@gmail.com>
2025-09-25 20:33:55 +0700

    feat: remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import About from "@/components/About";
-import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Services from "@/components/Services";
 
 export default function Home() {
   return (
@@ -15,9 +12,6 @@ export default function Home() {
       <main>
         <Hero />
         <Features />
-        {/* <About /> */}
-        {/* <Services /> */}
-        {/* <Contact /> */}
       </main>
       <Footer />
     </div>
